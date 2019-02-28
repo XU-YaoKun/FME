1dd81b7dae964de8fab99545c01138d31aea6d03
XXXU <xu_yaokun@163.com>
2019-02-28 13:50:06 -0800

    [Ref] dataloader

--- data/data.py
+++ data/data.py
@@ -14,9 +14,18 @@ import numpy as np
 import cv2
 
 from config import get_config
+from loader import KittiLoader
 
 # get argument
 config, unparsed = get_config()
+
+data_loader = KittiLoader(config.dataset_dir,
+                          config.static_frames_file,
+                          config.test_scene_file,
+                          img_height=config.img_height,
+                          img_width=config.img_width)
+
+
 
 
 
