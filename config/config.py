40704abb989ee21c4d8b57f8e3d9c385e760c3ef
XXXU <xu_yaokun@163.com>
2019-03-25 11:06:22 +0800

    [Add] classification loss and essential matrix loss

--- config/config.py
+++ config/config.py
@@ -37,6 +37,8 @@ obj_arg.add_argument("--top_k", type=int, default=2000, help="number of keypoint
 # -----------------------------------------------------------------------------
 # Loss
 loss_arg = add_argument_group("Loss")
+loss_arg.add_argument("--loss_classif", type=float, default=1.0, help="weight for classification loss")
+loss_arg.add_argument("--loss_essential", type=float, default=0.1, help="weight for essential matrix loss")
 
 # -----------------------------------------------------------------------------
 # Training
--- fme/models/LGR/loss.py
+++ fme/models/LGR/loss.py
@@ -3,14 +3,80 @@
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+
+def skew(v):
+    zero = torch.zeros_like(v[:,0])
+
+    M = torch.stack([
+        zero, -v[:,2], v[:,1],
+        v[:,2], zero, -v[:,0],
+        -v[:,1], v[:,0], zero,
+    ])
+
+    return M
 
 class LGR_loss(nn.Module):
     """
     loss function for LGR
     INCLUDING classification loss, essential matrix loss
     """
-    def __init__(self):
+    def __init__(self, threshold, classif_weight, essential_weight):
         super(LGR_loss, self).__init__()
+        self.threshold = threshold
+        self.classif_weight = classif_weight
+        self.essential_weight = essential_weight
 
-    def forward():
+    def forward(self, databatch, preds):
+        x_in = databatch["x_in"]
+        y_in = databatch["y_in"]
+        R_in = databatch["R"]
+        t_in = databatch["t"]
+
+        essential = preds["essential"]
+        logits = preds["logits"]
+
+        batch_size = x_in.size(0)
+        num_point = x_in.size(1)
+
+        # prepare for classification loss
+        geometric_distance = y_in[:,:,0]
+
+        # prepare for essential matrix loss
+        translate = skew(t_in).view(batch_size, 3, 3)
+        rotation = R_in.view(batch_size, 3, 3)
+        truth_essential = torch.matmul(translate, rotation).view(batch_size, 9)
+        truth_essential /= torch.norm(truth_essential, axis=1, keepdims=True)
+        
+        # essential matrix loss
+        essential_loss = torch.reduce_mean(torch.minimum(
+            torch.reduce_sum(torch.square(essential - truth_essential)),
+            torch.reduce_sum(torch.square(essential + truth_essential)),
+        ))
+
+        # classification loss
+        pos = torch.to_float(geometric_distance < self.threshold)
+        neg = torch.to_float(geometric_distance > self.threshold)
+        c = pos - neg
+        classification_loss = -torch.log(nn.sigmod(c * logits))
+
+        # balance
+        num_pos = torch.sum(pos, 1)
+        num_neg = torch.sum(neg, 1)
+
+        classification_loss_p = torch.sum(classification_loss * pos, 1)
+        classification_loss_n = torch.sum(classification_loss * neg, 1)
+
+        classification_loss_balance = torch.sum(classification_loss_p * 0.5 / num_pos + classification_loss_n * 0.5 / num_neg)
+
+        precision = (torch.sum((logits > 0).float() * pos, 1) / torch.sum((logits < 0).float * neg, 1)).mean()
+        recall = (torch.sum((logits > 0).float * pos, 1) / num_pos).mean() 
+
+        # not add l2 loss now, but maybe needed in the future
+
+        # add classification loss and essential matrix loss [and l2 loss]
+        loss = self.classif_weight * classification_loss + self.essential_weight * essential_loss
+
+        return loss
+
         
